5f07825ceb2d8f3814f93654a73bf41d27160394
WTHIJ <119924362+WTHIJ@users.noreply.github.com>
2026-08-19 10:17:38 +0200

    [fix] Fixed next config setting the ouput option to export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: "/landing_page",
   assetPrefix: "/landing_page/",
   images: {
